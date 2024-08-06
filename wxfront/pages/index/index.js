f3ff3bf558ac701b1a4a1d470186c73a1ecddade
tangou <tangou0789@163.com>
2024-08-06 09:07:07 +0800

    攻略静态暂时未完成

--- wxfront/pages/index/index.js
+++ wxfront/pages/index/index.js
@@ -11,4 +11,25 @@ Page({
   onShow() {
     this.getTabBar().init()
   },
+  onLoad: function () {
+    // 初始化时获取用户当前位置
+    this.getUserLocation()
+  },
+  getUserLocation: function () {
+    const vm = this
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        vm.setData({
+          longitude: res.longitude,
+          latitude: res.latitude,
+        })
+      },
+    })
+  },
+  moveToLocation: function () {
+    // 调用地图组件的移动到当前定位点
+    const mapCtx = wx.createMapContext('map')
+    mapCtx.moveToLocation()
+  },
 })
